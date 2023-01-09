46052b14ae6d545064a79405ad58a881882d5e17
egorfuntusov <funtushan@gmail.com>
2023-01-09 22:07:16 +0200

    about Collection and anonim class

---
+++ unit_7_collection/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,9 @@
+package ua.com.alevel;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        ListTest.test();
+        AnonimTest.test();
+    }
+}
---
+++ unit_7_collection/src/main/java/ua/com/alevel/MapTest.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapTest {
+
+    private static final Map<Integer, String> hashMap = new HashMap<>();
+    private static final Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+    private static final Map<Integer, String> treeMap = new TreeMap<>();
+}
