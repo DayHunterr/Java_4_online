c3cf62de8b1f1bbbb20949226f412e016b0a94e5
egorfuntusov <funtushan@gmail.com>
2023-01-26 21:48:19 +0200

    about io

---
+++ unit_11_ionio/src/main/java/ua/com/alevel/Student.java
@@ -0,0 +1,53 @@
+package ua.com.alevel;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private transient String fullName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
