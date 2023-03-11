529444b2b1437e793a0aca15486182db97f70963
egorfuntusov <funtushan@gmail.com>
2023-03-11 07:35:13 +0200

    add feedback

--- hw_11_calendar/src/main/java/ua/com/alevel/CalendarMain.java
+++ hw_11_calendar/src/main/java/ua/com/alevel/CalendarMain.java
@@ -5,9 +5,7 @@ import java.text.ParseException;
 
 public class CalendarMain {
     public static void main(String[] args) throws ParseException, IOException {
-
         CalendarController calendarController = new CalendarController();
         calendarController.start();
-
     }
 }
--- hw_module_2/src/main/java/ua/com/alevel/Main.java
+++ hw_module_2/src/main/java/ua/com/alevel/Main.java
@@ -1,6 +1,5 @@
 package ua.com.alevel;
 
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -10,55 +9,59 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Main {
-    private static File INPUT_PATH = new File("../SourcePath/input.json");
-    private static File OUTPUT_PATH = new File("../SourcePath/output.json");
-    private static Graph getGraph(JsonArray citiesJson){
+    private static final File INPUT_PATH = new File("../SourcePath/input.json");
+    private static final File OUTPUT_PATH = new File("../SourcePath/output.json");
+
+    private static Graph getGraph(JsonArray citiesJson) {
         Graph graph = new Graph();
-        for(int i = 0; i < citiesJson.size();i++){
+        for (int i = 0; i < citiesJson.size(); i++) {
             JsonObject city = citiesJson.get(i).getAsJsonObject();
             String name = city.get("name").getAsString();
             graph.addName(name);
             JsonArray neighbours = city.getAsJsonArray("neighbors");
-            for(int j = 0; j < neighbours.size();j++){
+            for (int j = 0; j < neighbours.size(); j++) {
                 JsonObject neighbour = neighbours.get(j).getAsJsonObject();
-                int to = neighbour.get("to").getAsInt() -1;
+                int to = neighbour.get("to").getAsInt() - 1;
                 int cost = neighbour.get("cost").getAsInt();
-                graph.addEdge(name,citiesJson.get(to).getAsJsonObject().get("name").getAsString(),cost);
+                graph.addEdge(name, citiesJson.get(to).getAsJsonObject().get("name").getAsString(), cost);
             }
         }
         return graph;
     }
-    private static JsonArray getShortestPath(JsonArray pathsJson,Graph graph){
+
+    private static JsonArray getShortestPath(JsonArray pathsJson, Graph graph) {
         JsonArray output = new JsonArray();
-        for(int i = 0; i < pathsJson.size();i++){
+        for (int i = 0; i < pathsJson.size(); i++) {
             JsonObject path = pathsJson.get(i).getAsJsonObject();
             String start = path.get("start").getAsString();
             String end = path.get("end").getAsString();
-            int cost = graph.DPQ(start,end);
+            int cost = graph.DPQ(start, end);
             output.add(cost);
         }
         return output;
     }
-    private static void writeToJson(Gson gson,JsonArray output){
+
+    private static void writeToJson(Gson gson, JsonArray output) {
         JsonObject result = new JsonObject();
-        result.add("costs",output);
-        try(FileWriter fileWriter = new FileWriter(OUTPUT_PATH)){
-            gson.toJson(result,fileWriter);
-        }catch (Exception e){
+        result.add("costs", output);
+        try (FileWriter fileWriter = new FileWriter(OUTPUT_PATH)) {
+            gson.toJson(result, fileWriter);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         try {
             Gson gson = new Gson();
-            JsonArray jsonArray = gson.fromJson(new FileReader(INPUT_PATH),JsonArray.class);
+            JsonArray jsonArray = gson.fromJson(new FileReader(INPUT_PATH), JsonArray.class);
             JsonObject jsonObject = jsonArray.get(0).getAsJsonObject();
             JsonArray citiesJson = jsonObject.getAsJsonArray("cities");
             JsonArray pathsJson = jsonObject.getAsJsonArray("paths");
             Graph graph = getGraph(citiesJson);
-            JsonArray output = getShortestPath(pathsJson,graph);
-            writeToJson(gson,output);
-        } catch (Exception e){
+            JsonArray output = getShortestPath(pathsJson, graph);
+            writeToJson(gson, output);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
